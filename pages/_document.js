f93f7a25fe5532717030d6d8d196668dad162200
Ankush <ankush357159@gmail.com>
2022-07-16 23:54:57 +0530

    _document

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
           />
 
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
           <link
             href='https://fonts.googleapis.com/css2?family=Oleo+Script:wght@700&display=swap'
             rel='stylesheet'
